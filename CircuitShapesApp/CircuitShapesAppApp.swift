9eaa1bf5b94a7c04d3804ad523396d052de880a3
Radio Engineering <shahul-hameed_shaik@astro.com.my>
2025-03-11 14:52:08 -0400

    Initial Commit

---
+++ CircuitShapesApp/CircuitShapesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CircuitShapesAppApp.swift
+//  CircuitShapesApp
+//
+//  Created by Shahul Hameed Shaik on 11/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct CircuitShapesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CircuitShapesApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CircuitShapesApp
+//
+//  Created by Shahul Hameed Shaik on 11/03/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
